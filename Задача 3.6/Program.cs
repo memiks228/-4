d6f07c312f36674a6c75550a136e5bfb2c4134c9
memiks228 <10memiks@mail.ru>
2025-04-12 17:28:04 +0400

    Add files via upload

---
+++ Задача 3.6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Задача_3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 1; i <= 11; i += 2)
+            {
+                double term = Math.Pow(x, i) / i;
+                sum += term;
+            }
+
+            Console.WriteLine($"Сумма: {sum}");
+        }
+    }
+}
---
+++ задача 3.5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace задача_3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (i % 2 == 0 ? -1 : 1) / (double)i;
+            }
+
+            Console.WriteLine($"Сумма: {sum}");
+        }
+    }
+}
