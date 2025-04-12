a0eb2c6d408f9ae1312cad981d16b5c0333a9726
memiks228 <10memiks@mail.ru>
2025-04-12 17:26:27 +0400

    Add files via upload

---
+++ Задача 3.3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Задача_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A
+            //for (int i = 20; i <= 35; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //    int b = int.Parse(Console.ReadLine());
+
+            //    for (int i = 10; i <= b; i++)
+            //    {
+            //        Console.WriteLine(i * i);
+            //    }
+            //}
+            //B
+
+
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = a; i <= 50; i++)
+            //{
+            //    Console.WriteLine(i * i * i);
+            //}
+            //Г
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = b; i <= a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
